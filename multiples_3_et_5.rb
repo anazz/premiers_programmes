1892a5f67a17181c08b983585997f24b1b721ac1
Ana Zegheanu <ana.zegheanu@gmail.com>
2018-04-10 18:01:54 +0200

    Update multiples_3_et_5.rb

--- multiples_3_et_5.rb
+++ multiples_3_et_5.rb
@@ -1,10 +1,8 @@
-##### code Ana Zegheanu #####
-
 =begin
 	### cette methode va additioner les multiples de 3 et 5 jusqu'à 1000 ###	
 =end
 
-def multiples_3_and_5
+def multiples_3_5
   #  on met a sum la valeur de depart 0
   sum = 0
   #  on fait une iteration avec each.do sur les valeurs de 1 à 1000
